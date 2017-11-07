bf8200fce97e2aa64702ae47011fa7fe4d9f16c1
8_n3z <jesus.saavedra37926637@gmail.com>
2017-11-06 21:46:26 -0700

    working list with id tags as well

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import SuperHeroes from "./superHeroes";
 
 class App extends Component {
+  state = {
+    title: "Super heroes",
+    heroes: SuperHeroes.HEROES,
+    selectedHero: {}
+  };
+
+  handleSelectedHero = hero => {
+    this.setState({
+      selectedHero: hero
+    });
+  };
+
   render() {
+    const HeroesList = this.state.heroes.map(hero => {
+      return (
+        <li key={hero.id} onClick={() => this.handleSelectedHero(hero)}>
+          <span className="badge">{hero.id}:</span> {hero.superhero}
+        </li>
+      );
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.title}</h1>
+        <ul className="heroes">{HeroesList}</ul>
+        <div>
+          <div>
+            <label>ID:</label>
+            {this.state.selectedHero.id}
+          </div>
+          <form>
+            <label>Hero Name:</label>
+            <input type="text" value={this.state.selectedHero.superhero} />
+            <input className="button" type="submit" value="submit" />
+          </form>
+        </div>
       </div>
     );
   }
